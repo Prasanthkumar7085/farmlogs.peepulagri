ae08bf372a7448dc39251dee0fbf27ad208eb1d7
kishore <kishore.r@orotron.com>
2023-09-08 17:10:08 +0530

    Favicon Image Updated

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+// pages/_document.tsx
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+            {/* Add your favicon link tag here */}
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" type="image/png" href="/favicon.png" ></link>
+            <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes='32x32'></link>
+          {/* Additional head elements can be added here */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

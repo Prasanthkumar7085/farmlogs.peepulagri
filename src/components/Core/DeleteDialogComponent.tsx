4663b7ae9f37d56b4132c79edc19cff10f064aef
kishore <kishore.r@orotron.com>
2023-09-13 09:44:09 +0530

    Updated Delete Log Dialog

--- src/components/Core/DeleteDialogComponent.tsx
+++ src/components/Core/DeleteDialogComponent.tsx
@@ -1,4 +1,6 @@
-import { Button, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material"
+import { Button, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Typography } from "@mui/material"
+import ReportProblemIcon from '@mui/icons-material/ReportProblem';
+import Styles from './DeleteDialogComponent.module.css';
 
 const DeleteDialogCompoennt = ({ deleteContent, deleteDialogOpen, confirmDelete }: any) => {
     return (
@@ -6,19 +8,27 @@ const DeleteDialogCompoennt = ({ deleteContent, deleteDialogOpen, confirmDelete 
         <Dialog
             open={deleteDialogOpen}
             onClose={() => confirmDelete(false, '')}
+            className={Styles.deleteDialog}
         >
-            <DialogTitle id="alert-dialog-title" sx={{ color: "red" }}>
-                {"Delete The Log!"}
+            <DialogTitle id="alert-dialog-title">
+                <div>
+                    <Typography variant="h5" color="secondary" className={Styles.title}>
+                        <ReportProblemIcon />
+                        <span>
+                            DELETE LOG
+                        </span> 
+                    </Typography>              
+                </div>
             </DialogTitle>
             <DialogContent>
                 <DialogContentText id="alert-dialog-description">
-                    <p style={{ fontSize: "20px" }}> {`Do you really wanna delete the log `}</p>
-                    <p style={{ fontSize: "15px", color: "blue" }}>{`${deleteContent?.title}`}</p>
+                    <Typography> {`Do you really wanna delete the log `}</Typography>
+                    <Typography variant="h6" color="primary">{`${deleteContent?.title}`}</Typography>
                 </DialogContentText>
             </DialogContent>
             <DialogActions>
-                <Button onClick={() => confirmDelete(false, '')}>Cancel</Button>
-                <Button onClick={() => confirmDelete(true, deleteContent?._id)} autoFocus>
+                <Button variant="contained" className={Styles.DefaultBtn} onClick={() => confirmDelete(false, '')}>Cancel</Button>
+                <Button variant="contained" color="primary" onClick={() => confirmDelete(true, deleteContent?._id)} autoFocus>
                     Delete
                 </Button>
             </DialogActions>

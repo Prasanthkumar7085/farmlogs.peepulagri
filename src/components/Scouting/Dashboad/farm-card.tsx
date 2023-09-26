e5978f73210a510cb4de521c18b0f1b395233673
sai <sai.j@orotron.com>
2023-09-26 16:54:40 +0530

    sw/dashboard page form cards

--- src/components/Scouting/Dashboad/farm-card.tsx
+++ src/components/Scouting/Dashboad/farm-card.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import { useCallback } from "react";
 import styles from "./farm-card.module.css";
+import Image from "next/image";
 
 const FarmCard: NextPage = () => {
   const onFarmCardClick = useCallback(() => {
@@ -24,11 +25,152 @@ const FarmCard: NextPage = () => {
               </div>
             </div>
             <div className={styles.viewfarm} id="icon-button-view-farm">
-              <img className={styles.buttonIcon} alt="" src="/button.svg" />
+              <img alt="" src="/form-card-arrow-icon.svg" />
             </div>
           </div>
         </div>
       </div>
+      <div className={styles.farmcard} id="farm-card" onClick={onFarmCardClick}>
+        <div className={styles.farm} id="farm">
+          <div className={styles.farmdetails} id="farm-detalis">
+            <div className={styles.duration} id="duration">
+              <div className={styles.aug2023}>25, Aug 2023 - Current</div>
+            </div>
+            <div className={styles.farm1}>Farm-1</div>
+          </div>
+          <div className={styles.farmareablock} id="farm-area-block">
+            <div className={styles.farmarea} id="farm-area">
+              <div className={styles.area}>
+                <div className={styles.acres}>20 Acres</div>
+              </div>
+            </div>
+            <div className={styles.viewfarm} id="icon-button-view-farm">
+              <img alt="" src="/form-card-arrow-icon.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.farmcard} id="farm-card" onClick={onFarmCardClick}>
+        <div className={styles.farm} id="farm">
+          <div className={styles.farmdetails} id="farm-detalis">
+            <div className={styles.duration} id="duration">
+              <div className={styles.aug2023}>25, Aug 2023 - Current</div>
+            </div>
+            <div className={styles.farm1}>Farm-1</div>
+          </div>
+          <div className={styles.farmareablock} id="farm-area-block">
+            <div className={styles.farmarea} id="farm-area">
+              <div className={styles.area}>
+                <div className={styles.acres}>20 Acres</div>
+              </div>
+            </div>
+            <div className={styles.viewfarm} id="icon-button-view-farm">
+              <img alt="" src="/form-card-arrow-icon.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.farmcard} id="farm-card" onClick={onFarmCardClick}>
+        <div className={styles.farm} id="farm">
+          <div className={styles.farmdetails} id="farm-detalis">
+            <div className={styles.duration} id="duration">
+              <div className={styles.aug2023}>25, Aug 2023 - Current</div>
+            </div>
+            <div className={styles.farm1}>Farm-1</div>
+          </div>
+          <div className={styles.farmareablock} id="farm-area-block">
+            <div className={styles.farmarea} id="farm-area">
+              <div className={styles.area}>
+                <div className={styles.acres}>20 Acres</div>
+              </div>
+            </div>
+            <div className={styles.viewfarm} id="icon-button-view-farm">
+              <img alt="" src="/form-card-arrow-icon.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.farmcard} id="farm-card" onClick={onFarmCardClick}>
+        <div className={styles.farm} id="farm">
+          <div className={styles.farmdetails} id="farm-detalis">
+            <div className={styles.duration} id="duration">
+              <div className={styles.aug2023}>25, Aug 2023 - Current</div>
+            </div>
+            <div className={styles.farm1}>Farm-1</div>
+          </div>
+          <div className={styles.farmareablock} id="farm-area-block">
+            <div className={styles.farmarea} id="farm-area">
+              <div className={styles.area}>
+                <div className={styles.acres}>20 Acres</div>
+              </div>
+            </div>
+            <div className={styles.viewfarm} id="icon-button-view-farm">
+              <img alt="" src="/form-card-arrow-icon.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.farmcard} id="farm-card" onClick={onFarmCardClick}>
+        <div className={styles.farm} id="farm">
+          <div className={styles.farmdetails} id="farm-detalis">
+            <div className={styles.duration} id="duration">
+              <div className={styles.aug2023}>25, Aug 2023 - Current</div>
+            </div>
+            <div className={styles.farm1}>Farm-1</div>
+          </div>
+          <div className={styles.farmareablock} id="farm-area-block">
+            <div className={styles.farmarea} id="farm-area">
+              <div className={styles.area}>
+                <div className={styles.acres}>20 Acres</div>
+              </div>
+            </div>
+            <div className={styles.viewfarm} id="icon-button-view-farm">
+              <img alt="" src="/form-card-arrow-icon.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.farmcard} id="farm-card" onClick={onFarmCardClick}>
+        <div className={styles.farm} id="farm">
+          <div className={styles.farmdetails} id="farm-detalis">
+            <div className={styles.duration} id="duration">
+              <div className={styles.aug2023}>25, Aug 2023 - Current</div>
+            </div>
+            <div className={styles.farm1}>Farm-1</div>
+          </div>
+          <div className={styles.farmareablock} id="farm-area-block">
+            <div className={styles.farmarea} id="farm-area">
+              <div className={styles.area}>
+                <div className={styles.acres}>20 Acres</div>
+              </div>
+            </div>
+            <div className={styles.viewfarm} id="icon-button-view-farm">
+              <img alt="" src="/form-card-arrow-icon.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.farmcard} id="farm-card" onClick={onFarmCardClick}>
+        <div className={styles.farm} id="farm">
+          <div className={styles.farmdetails} id="farm-detalis">
+            <div className={styles.duration} id="duration">
+              <div className={styles.aug2023}>25, Aug 2023 - Current</div>
+            </div>
+            <div className={styles.farm1}>Farm-1</div>
+          </div>
+          <div className={styles.farmareablock} id="farm-area-block">
+            <div className={styles.farmarea} id="farm-area">
+              <div className={styles.area}>
+                <div className={styles.acres}>20 Acres</div>
+              </div>
+            </div>
+            <div className={styles.viewfarm} id="icon-button-view-farm">
+              <img alt="" src="/form-card-arrow-icon.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };

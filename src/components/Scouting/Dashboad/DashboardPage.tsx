bf6ed2b02351821268ff566b37024e9650724b8c
prasanth_oro <prasanth.m@orotron.com>
2023-09-26 17:12:05 +0530

    addform page

--- src/components/Scouting/Dashboad/DashboardPage.tsx
+++ src/components/Scouting/Dashboad/DashboardPage.tsx
@@ -1,13 +1,15 @@
+import { useRouter } from "next/router";
 import DashBoardHeader from "./dash-board-header";
 import FarmCard from "./farm-card";
 
 const DashboardPage = () => {
+    const router = useRouter()
     return (
         <div id="dashboardPage">
             <DashBoardHeader />
             <FarmCard />
             <div className="addFormPositionIcon">
-                <img src="/add-form-icon.svg" alt="" />
+                <img src="/add-form-icon.svg" alt="" onClick={() => router.push("/scouting/forms/add")} />
             </div>
         </div>
     );

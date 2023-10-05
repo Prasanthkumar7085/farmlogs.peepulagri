bd1dbe4e4b55948b69c0396209ccd99beac8e768
fe-dev2 <fe-dev2@labsquire.com>
2023-10-05 15:43:01 +0530

    sw/no data for crops and scouting web

--- src/components/ScoutingWeb/Crops/FolderCard.tsx
+++ src/components/ScoutingWeb/Crops/FolderCard.tsx
@@ -5,23 +5,23 @@ import { useRouter } from "next/router";
 import { CropTypeResponse } from "@/types/cropTypes";
 import timePipe from "@/pipes/timePipe";
 
-interface pageProps{
+interface pageProps {
     cropsData: Array<CropTypeResponse>;
     loading: boolean;
 }
-const FolderStructure = ({ cropsData,loading}:pageProps) => {
+const FolderStructure = ({ cropsData, loading }: pageProps) => {
 
     const router = useRouter();
-    
+
     const onFolderStructureContainerClick = useCallback((cropId: string) => {
         console.log(router.query.farm_id);
-        
+
         router.push(`/farm/${router.query.farm_id}/crops/${cropId}/scouting`);
     }, []);
 
     return (
         <div className={styles.allCropsCardContainer}>
-            {cropsData?.length ? cropsData.map((item:CropTypeResponse,index:number) => {
+            {cropsData.map((item: CropTypeResponse, index: number) => {
                 return (
                     <div
                         key={index}
@@ -35,15 +35,15 @@ const FolderStructure = ({ cropsData,loading}:pageProps) => {
                             </div>
                         </div>
                         <div className={styles.textwrapper}>
-                            <h6 className={styles.type}>{item.title ? item.title :""}</h6>
-                            <div style={{display:"flex",justifyContent:"space-between",width:"100%"}}>
+                            <h6 className={styles.type}>{item.title ? item.title : ""}</h6>
+                            <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
                                 <div className={styles.date}>{timePipe(item.createdAt, 'DD, MMM YYYY')}</div>
-                                <div className={styles.date}>{item.crop_area ? item.crop_area : 0} {item.crop_area > 1? 'acres':'acre'}</div>
+                                <div className={styles.date}>{item.crop_area ? item.crop_area : 0} {item.crop_area > 1 ? 'acres' : 'acre'}</div>
                             </div>
                         </div>
-                    </div>              
+                    </div>
                 )
-            }):!loading?"No Crops for the selected farm":""}
+            })}
         </div>
     );
 };

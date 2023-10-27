f0f63d7dd7f32d865852ace07db1a13ab6cc0550
prasanth_oro <prasanth.m@orotron.com>
2023-10-27 18:21:24 +0530

    Webpack svgr added

--- next.config.js
+++ next.config.js
@@ -7,12 +7,21 @@ const nextConfig = {
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     },
     images: {
-        remotePatterns: [            
+        remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'peepul-agri-dev.s3.ap-south-1.amazonaws.com',
             },
         ],
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        })
+
+        return config
     },
 
 }

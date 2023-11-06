e324214ff74098e4764e7f8417f9071da08b4df0
fe-dev2 <fe-dev2@labsquire.com>
2023-11-06 14:48:42 +0530

    sw/single image preview

--- src/components/Tasks/AllTasks/TasksTable/AttachmentDrawer.tsx
+++ src/components/Tasks/AllTasks/TasksTable/AttachmentDrawer.tsx
@@ -2,11 +2,13 @@
 import { removeTheAttachementsFilesFromStore } from "@/Redux/Modules/Conversations";
 import timePipe from "@/pipes/timePipe";
 import CloseIcon from "@mui/icons-material/Close";
-import { CircularProgress, IconButton, Typography } from "@mui/material";
+import { CircularProgress, Dialog, IconButton, Typography } from "@mui/material";
 import Drawer from "@mui/material/Drawer";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "../../TaskComments/Comments.module.css";
+import Image from "next/image";
+import { Close } from "@mui/icons-material";
 const AttachmentDrawerTaskmodule = ({
     attachmentDrawerClose,
     rowDetails,
@@ -19,38 +21,9 @@ const AttachmentDrawerTaskmodule = ({
 
     const [loading, setLoading] = useState<any>();
     const [attachmentData, setAttachmentData] = useState<any>();
-    // const mockData = [
-    //     {
-    //         name: "2023-11-06_054518_background-img.png",
-    //         original_name: "background-img.png",
-    //         type: "image/png",
-    //         size: 4032851,
-    //         path: "65237516de9193d24c05b8f1/2023-11-06_054518_background-img.png",
-    //         time: "2023-11-04T05:45:58.988Z",
-    //         _id: "65487d9664c9aec9abc27c9b",
-    //         url: "https://peepul-agri-dev.s3.ap-south-1.amazonaws.com/6520e0de9a170d60fa925b6d/tasks/65237516de9193d24c05b8f1/2023-11-06_054518_background-img.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIATCP4XX4BIG3O2JMK%2F20231106%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20231106T062249Z&X-Amz-Expires=3600&X-Amz-Signature=fb3a9debccc2524d444eb1bec6a5140cc8158a524fd515a79811fd1a386044c9&X-Amz-SignedHeaders=host",
-    //     },
-    //     {
-    //         name: "2023-11-06_054555_mr-yoda-1.png",
-    //         original_name: "Mr-Yoda 1.png",
-    //         type: "image/png",
-    //         size: 32449,
-    //         path: "65237516de9193d24c05b8f1/2023-11-06_054555_mr-yoda-1.png",
-    //         time: "2023-11-04T05:45:58.988Z",
-    //         _id: "65487d9664c9aec9abc27c9c",
-    //         url: "https://peepul-agri-dev.s3.ap-south-1.amazonaws.com/6520e0de9a170d60fa925b6d/tasks/65237516de9193d24c05b8f1/2023-11-06_054555_mr-yoda-1.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIATCP4XX4BIG3O2JMK%2F20231106%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20231106T062249Z&X-Amz-Expires=3600&X-Amz-Signature=6e87d7a1977ab1f66afb0dda13117429675f047b55ffcd364c8d91ea8099288a&X-Amz-SignedHeaders=host",
-    //     },
-    //     {
-    //         name: "2023-11-06_054536_mr-yoda-white.png",
-    //         original_name: "MR-yoda-white.png",
-    //         type: "image/png",
-    //         size: 5092,
-    //         path: "65237516de9193d24c05b8f1/2023-11-06_054536_mr-yoda-white.png",
-    //         time: "2023-11-03T05:45:58.988Z",
-    //         _id: "65487d9664c9aec9abc27c9d",
-    //         url: "https://peepul-agri-dev.s3.ap-south-1.amazonaws.com/6520e0de9a170d60fa925b6d/tasks/65237516de9193d24c05b8f1/2023-11-06_054536_mr-yoda-white.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIATCP4XX4BIG3O2JMK%2F20231106%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20231106T062249Z&X-Amz-Expires=3600&X-Amz-Signature=91653e6f2e35b715db6c8e3f247fd3ead49ff93b2b096cb4fb2480d681cb6942&X-Amz-SignedHeaders=host",
-    //     },
-    // ];
+    const [singleImageView, setSingleImageView] = useState<any>(false);
+    const [imageid, setImageId] = useState<any>();
+
 
 
     function groupByDate(array: Array<any>) {
@@ -83,10 +56,6 @@ const AttachmentDrawerTaskmodule = ({
             let responseData = await response.json();
             if (responseData.status >= 200 && responseData.status <= 300) {
                 let modifiedData = groupByDate(responseData?.data?.attachments);
-                // let modifiedData = groupByDate([
-                //     ...responseData?.data?.attachments,
-                //     ...mockData,
-                // ]);
                 setAttachmentData(modifiedData);
             }
         } catch (err) {
@@ -104,62 +73,88 @@ const AttachmentDrawerTaskmodule = ({
     }, [attachmentdrawer]);
 
     return (
-        <Drawer
-            anchor="right"
-            open={attachmentdrawer}
-            sx={{
-                "& .MuiPaper-root": {
-                    padding: "1rem",
-                    minWidth: "600px",
-                    maxWidth: "600px",
-                },
-            }}
-        >
-            <div className={styles.drawerHeader}>
-                <Typography variant="h6">Attachments</Typography>
-                <IconButton
-                    onClick={() => {
-                        attachmentDrawerClose();
-                        dispatch(removeTheAttachementsFilesFromStore([]));
-                    }}
-                >
-                    <CloseIcon sx={{ color: "#000" }} />
-                </IconButton>
-            </div>
+        <div>
 
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    paddingBlock: "1rem"
+            <Drawer
+                anchor="right"
+                open={attachmentdrawer}
+                sx={{
+                    "& .MuiPaper-root": {
+                        padding: "1rem",
+                        minWidth: "600px",
+                        maxWidth: "600px",
+                    },
                 }}
             >
-                {(!loading && attachmentData?.length) ? attachmentData?.map((item: any, index: any) => {
-                    return (
-                        <div style={{ marginBottom: "1rem" }}>
-                            <p className={styles.AttachmentDate}>{timePipe(item[0]?.time, "DD MMM YYYY, hh:mm A")}</p>
-                            <div className={styles.attachmentDrawer}>
-                                {item?.map((image: any, index: number) => {
-                                    return (
-                                        <div key={index} className={styles.taskModuleAttachmentBlock}>
-                                            <img src={image?.url} alt="" height={100}
-                                                width={100} className={styles.attachmentImg} />
-                                            <div className={styles.viewIcon}>
-                                                <img src="/view-icon-task.svg"
-                                                    height={30}
-                                                    width={30}
-                                                    alt="view" />
-                                            </div>
-                                        </div>
-                                    )
-                                })}
+                <div className={styles.drawerHeader}>
+                    <Typography variant="h6">Attachments</Typography>
+                    <IconButton
+                        onClick={() => {
+                            attachmentDrawerClose();
+                            dispatch(removeTheAttachementsFilesFromStore([]));
+                        }}
+                    >
+                        <CloseIcon sx={{ color: "#000" }} />
+                    </IconButton>
+                </div>
 
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        paddingBlock: "1rem"
+                    }}
+                >
+                    {(!loading && attachmentData?.length) ? attachmentData?.map((item: any, index: any) => {
+                        return (
+                            <div style={{ marginBottom: "1rem" }}>
+                                <p className={styles.AttachmentDate}>{timePipe(item[0]?.time, "DD MMM YYYY, hh:mm A")}</p>
+                                <div className={styles.attachmentDrawer}>
+                                    {item?.map((image: any, index: number) => {
+                                        return (
+                                            <div key={index} className={styles.taskModuleAttachmentBlock}>
+                                                <img src={image?.url} alt="" height={100}
+                                                    width={100} className={styles.attachmentImg} />
+                                                <div className={styles.viewIcon} onClick={() => {
+                                                    setSingleImageView(true)
+                                                    setImageId(image)
+                                                }}>
+                                                    <img src="/view-icon-task.svg"
+                                                        height={30}
+                                                        width={30}
+                                                        alt="view" />
+                                                </div>
+                                            </div>
+                                        )
+                                    })}
+
+                                </div>
                             </div>
-                        </div>
-                    )
-                }) : (loading ? <CircularProgress /> : "No Attachments")}
-            </div>
-        </Drawer >
+                        )
+                    }) : (loading ? <CircularProgress /> : "No Attachments")}
+                </div>
+            </Drawer >
+            <Dialog
+                open={singleImageView}
+                fullScreen
+                sx={{
+                    "& .MuiPaper-root": {
+                        background: "#00000063",
+                        padding: "1rem",
+
+                    },
+                }}
+            >
+                <div>
+                    <div style={{ textAlign: "end", cursor: "pointer" }} onClick={() => setSingleImageView(false)}>
+                        <Close sx={{ color: "#fff", fontSize: "2.5rem" }} />
+                    </div>
+                    <div className={styles.singleImageDialog} >
+                        <img src={imageid?.url} alt="" />
+                    </div>
+                </div>
+            </Dialog>
+        </div>
     );
 };
 export default AttachmentDrawerTaskmodule;

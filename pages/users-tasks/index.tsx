001e5277772bb0891707b97559ce0c2959c317e7
venugopalkri <gopal.t@orotron.com>
2023-12-19 14:32:02 +0530

    vn/Add task api integration in mobile response

---
+++ pages/users-tasks/index.tsx
@@ -0,0 +1,12 @@
+import AllTasks from "@/components/Tasks/MobileTasksComponents/AllTasks";
+
+
+const TaskPage = () => {
+
+    return (
+        <div style={{ width: "100%", maxWidth: "500px", margin: "0 auto" }}>
+            <AllTasks />
+        </div>
+    );
+}
+export default TaskPage;

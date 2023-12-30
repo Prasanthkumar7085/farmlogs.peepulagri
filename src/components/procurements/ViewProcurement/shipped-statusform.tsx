2d116256f11c1361d90fbe34e9e2605a536e5e7a
prasanth_oro <prasanth.m@orotron.com>
2023-12-30 08:13:50 +0530

    updated changes in the tracking module

--- src/components/procurements/ViewProcurement/shipped-statusform.tsx
+++ src/components/procurements/ViewProcurement/shipped-statusform.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
-import { Button, Tooltip } from "@mui/material";
+import { Button, Tooltip, Typography } from "@mui/material";
 import styles from "./shipped-statusform.module.css";
 import timePipe from "@/pipes/timePipe";
+import { useState } from "react";
 
 // type ShippedStatusformType = {
 //   fARM1?: string;
@@ -12,6 +13,24 @@ import timePipe from "@/pipes/timePipe";
 // };
 
 const ShippedStatusform = ({ data }: any) => {
+
+  const [showTooltip, setShowTooltip] = useState<any>(false);
+
+  const copyTextToClipboard = () => {
+    navigator.clipboard.writeText(data?.tracking_details?.tracking_id)
+      .then(() => {
+        console.log("Text copied")
+        setShowTooltip(true);
+        setTimeout(() => {
+          setShowTooltip(false);
+        }, 2000);
+      })
+      .catch(err => {
+        console.error('Unable to copy text: ', err);
+      });
+  };
+
+
   return (
     <div className={styles.shippedstatusform}>
       <Tooltip
@@ -22,12 +41,12 @@ const ShippedStatusform = ({ data }: any) => {
         <div className={styles.farmname}>
           {data?.farm_ids.length < 3
             ? data?.farm_ids
-                ?.map((item: any, index: any) => item.title)
-                .join(",")
+              ?.map((item: any, index: any) => item.title)
+              .join(",")
             : data?.farm_ids
-                .slice(0, 2)
-                ?.map((item: any, index: any) => item.title)
-                .join(",") + "....."}
+              .slice(0, 2)
+              ?.map((item: any, index: any) => item.title)
+              .join(",") + "....."}
         </div>
       </Tooltip>
       <div className={styles.nameofoperation}>
@@ -47,17 +66,29 @@ const ShippedStatusform = ({ data }: any) => {
           {data?.point_of_contact?.name ? data?.point_of_contact?.name : "----"}
         </h3>
       </div>
-      {/* <div className={styles.trackingid}>
-        <label className={styles.label1}>Tracking ID</label>
-        <div className={styles.information}>
-          <div className={styles.id}>
-            <p className={styles.text}>{"1234567890p"}</p>
+      {data?.tracking_details?.tracking_id ?
+        <div className={styles.trackingid}>
+          <Typography variant="h6" color="ThreeDLightShadow">Tracking Details</Typography>
+          <label className={styles.label1}>Tracking ID</label>
+          <div className={styles.information}>
+            <div className={styles.id}>
+              <p className={styles.text}>{data?.tracking_details?.tracking_id}</p>
+            </div>
+
+            {showTooltip ?
+              <Tooltip title="Text copied!" >
+                <Button color="primary" variant="contained">
+                  Copy
+                </Button>
+              </Tooltip> :
+              <Button color="primary" variant="contained" onClick={() => copyTextToClipboard()}>
+                Copy
+              </Button>
+            }
           </div>
-          <Button color="primary" variant="contained">
-            Copy
-          </Button>
-        </div>
-      </div> */}
+
+        </div> : ""}
+
     </div>
   );
 };
